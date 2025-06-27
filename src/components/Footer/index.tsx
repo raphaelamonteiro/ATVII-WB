3c8546a1499d0db2b6c8c7d5525c238dc2baad61
Raphaela Monteiro <bmraphaela@gmail.com>
2025-06-27 17:54:30 -0300

    ajustes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,27 +1,22 @@
 import React from 'react'
 import "./style.css";
 
-class Footer extends React.Component  {
+class Footer extends React.Component {
   render() {
     return (
-    <div className='container-footer'>
-      <h4>World Beauty</h4>
-      <div className='container-icons'>
-        <a href="https://github.com/DigoCast" target='_blank'>
-          <i className="fa-brands fa-github fa-xl" style={{color: "#401D10"}}></i>
-        </a>
-        <a href="https://www.instagram.com/diego_.cast/" target='_blank' >
-          <i className="fa-brands fa-instagram fa-xl" style={{color: "#401D10"}}></i>
-        </a>
-        <a href="https://www.linkedin.com/in/diego-castilho-8b87a8301/" target='_blank'>
-          <i className="fa-brands fa-linkedin fa-xl" style={{color: "#401D10"}}></i>
-        </a>
-        <a href="#">
-          <i className="fa-brands fa-whatsapp fa-xl" style={{color: "#401D10"}}></i>
-        </a>
+      <div className='container-footer'>
+        <h4>World Beauty</h4>
+        <div className='container-icons'>
+          <a href="https://github.com/raphaelamonteiro" target='_blank' rel="noreferrer">
+            <i className="fa-brands fa-github fa-xl" style={{ color: "#401D10" }}></i>
+          </a>
+          <a href="https://www.linkedin.com/in/raphaelamonteiro/" target='_blank' rel="noreferrer">
+            <i className="fa-brands fa-linkedin fa-xl" style={{ color: "#401D10" }}></i>
+          </a>
+
+        </div>
+        <p>Copyright© - 2025;Raphaela Monteiro </p>
       </div>
-      <p>Copyright© - 2025; Designed by Diego Castilho</p>
-    </div>
     )
   }
 }
